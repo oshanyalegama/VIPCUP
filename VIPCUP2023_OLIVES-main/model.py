88fd88d92ca1d692f17bb47be559408c81801266
oshanyalegama <110217717+oshanyalegama@users.noreply.github.com>
2023-08-27 15:45:32 +0530

    Update model.py

--- VIPCUP2023_OLIVES-main/model.py
+++ VIPCUP2023_OLIVES-main/model.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 import torchvision
+from models import ResNet50_Weights
 
 class ResNet(nn.Module):
     """encoder + classifier"""
     def __init__(self, name='resnet50', num_classes=2):
         super(ResNet, self).__init__()
         if (name == 'resnet50'):
-            self.encoder = torchvision.models.resnet50(zero_init_residual=True)
+            self.encoder = torchvision.models.resnet50(zero_init_residual=True, weights=ResNet50_Weights.IMAGENET1K_V2)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
